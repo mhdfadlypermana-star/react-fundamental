8d92a3f35aa54fd0dfc3b5f1eda2acde885ba790
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-06 17:37:44 +0700

    cek

--- my-project-use-effect/src/MyComponent.jsx
+++ my-project-use-effect/src/MyComponent.jsx
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
 
-function MyComponent(){
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("green");
-    
-    useEffect(() => {
-        document.title = `Count : ${count} ${color}`;
-    } );
+  useEffect(() => {
+    document.title = `Count : ${count} ${color}`;
+  });
 
-    function addCount(){
-        setCount(count => count + 1)
-    }
-    function subtractCount(){
-        setCount(count => count - 1)
-    }
-    function changeColor(){
-        setColor(color => color === "green" ? "red" : "green");
-    }
+  function addCount() {
+    setCount((count) => count + 1);
+  }
+  function subtractCount() {
+    setCount((count) => count - 1);
+  }
+  function changeColor() {
+    setColor((color) => (color === "green" ? "red" : "green"));
+  }
 
-    return(<>
-        <p style={{color: color}}>Count: {count}</p>
-        <button onClick={addCount}>Add</button>
-        <button onClick={subtractCount}>Subtract</button><br/>
-        <button onClick={changeColor}>Change Color</button>
-
-    </>)
+  return (
+    <>
+      <p style={{ color: color }}>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button>
+      <br />
+      <button onClick={changeColor}>Change Color</button>
+    </>
+  );
 }
 
 export default MyComponent;
