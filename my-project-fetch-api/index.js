7fb731f2f0b86745566109adcc8370921e17d0de
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-08 00:33:14 +0700

    add search a Pokemon

--- my-project-fetch-api/index.js
+++ my-project-fetch-api/index.js
@@ -1,5 +1,25 @@
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error) );
+
+// fetchData();
+
+async function fetchData() {
+    
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!response.ok) {
+            throw new Error("Datanya ga ketemu Bree");
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+    } catch (error) {
+        console.error(error);
+    }
+}
