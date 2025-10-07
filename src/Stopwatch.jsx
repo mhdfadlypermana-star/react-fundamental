ea26a63fd37b4b10c349824c97e6ddac9e1c35ff
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-07 11:13:54 +0700

    add style

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -27,10 +27,11 @@ function Stopwatch(){
     return(
         <div className='stopwatch'>
             <div className='display'>{formatTime()}</div>
+            <div className='controls'>
             <button onClick={start()} className='start-button'>Start</button>
             <button onClick={reset()} className='reset-button'>Reset</button>
             <button onClick={stop()} className='stop-button'>Stop</button>
-
+            </div>
         </div>
     );
 
