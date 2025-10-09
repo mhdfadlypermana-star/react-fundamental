71e2f64da5d57ec0eb1d76c210e89a7ea65111fe
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-09 16:51:22 +0700

    .

--- my-project-graphql/client/src/App.jsx
+++ my-project-graphql/client/src/App.jsx
@@ -1,44 +1,75 @@
-import { useQuery,useMutation,gql } from "@apollo/client"
+import { useQuery, gql } from "@apollo/client";
 
 const GET_USERS = gql`
-query GetUser {
-  getUsers {
-    id
-    age
-    name
-    isMarried
+  query GetUser {
+    getUsers {
+      id
+      age
+      name
+      isMarried
+    }
   }
-}
+`;
+
+const GET_USER_BY_ID = gql`
+  query GetUserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      age
+      name
+      isMarried
+    }
+  }
 `;
 
 function App() {
+  // ambil semua user
+  const {
+    data: getUsersData,
+    error: getUsersError,
+    loading: getUsersLoading,
+  } = useQuery(GET_USERS);
 
-  const {data, error, loading} = useQuery(GET_USERS);
-  
-  if(loading) return <p>Loading dulu bre...</p>;
+  // ambil user berdasarkan id (misal id=1)
+  const {
+    data: getUserByIdData,
+    error: getUserByIdError,
+    loading: getUserByIdLoading,
+  } = useQuery(GET_USER_BY_ID, {
+    variables: { id: "1" }, // <- WAJIB
+  });
 
-  if(error) return <p>Error ngab : {error.message}</p>;
+  if (getUsersLoading) return <p>Loading dulu bre...</p>;
+  if (getUsersError) return <p>Error ngab: {getUsersError.message}</p>;
 
-  return(
-  <>
-    <h1>Users</h1>
-    <div> 
-      {data.getUsers.map((user) => (
+  return (
+    <>
+      <h1>Users</h1>
+
       <div>
-        <p>Name: {user.name}</p>
-        <p>Age: {user.age}</p>
-        <p>Is Married: {user.isMarried ? "yes" : "no"}</p>
-
-
+        {getUserByIdLoading ? (
+          <p>Loading ninja...</p>
+        ) : getUserByIdError ? (
+          <p>Error ninja: {getUserByIdError.message}</p>
+        ) : (
+          <>
+            <h2>Ninja Pilihan</h2>
+            <p>Name: {getUserByIdData?.getUserById?.name}</p>
+          </>
+        )}
       </div>
 
-    ))} </div>
+      <div>
+        {getUsersData?.getUsers.map((user) => (
+          <div key={user.id}>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Is Married: {user.isMarried ? "yes" : "no"}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
-  
-  
-    
-  
 }
 
-export default App
+export default App;
