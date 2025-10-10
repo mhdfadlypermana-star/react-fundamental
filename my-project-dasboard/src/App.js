683bbe9faab0f486b295da479ad544a3ccbb928e
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-11 00:10:04 +0700

    add Routers

--- my-project-dasboard/src/App.js
+++ my-project-dasboard/src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React,{useEffect} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 const App = () => {
     return (
-        <h1 className="underline text-3x1">App</h1>
+        <div>
+            <BrowserRouter>
+            <div className="flex relative dark">
+
+            </div>
+            </BrowserRouter>
+        </div>
     );
 }
 
