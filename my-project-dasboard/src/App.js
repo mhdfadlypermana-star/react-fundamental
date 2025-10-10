83b16186af87671e1562b16927bb8a8052aec06c
Muhammad Fadly <mhdfadlypermana@gmail.com>
2025-10-11 01:39:33 +0700

    add navbar component

--- my-project-dasboard/src/App.js
+++ my-project-dasboard/src/App.js
@@ -1,18 +1,62 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-            <div className="flex relative dark">
-
+  const activeMenu = false;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-4xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: "grey", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div
+              className="w-70 fixed sidebar 
+                    dark:bg-secondary-dark-bg
+                    bg-white"
+            >
+              Sidebar
             </div>
-            </BrowserRouter>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div
+            className={`dark:bg-main-bg 
+                        bg-main-bg min-h-screen md:ml-72 w-full 
+                        ${activeMenu ? "md:ml-72" : " flex-2"}`}
+          >
+            <div
+              className="fixed md:static 
+                        bg-main-bg dark:bg-main-dark-bg 
+                        navbar w-full"
+            >
+              Navbar
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+                <Route path="/" element="ECommerce" />
+                <Route path="/ecommerce" element="ECommerce" />
+
+            </Routes>
+          </div>
+
         </div>
-    );
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
